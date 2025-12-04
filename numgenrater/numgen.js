36ed854e5f609c37a35657094ced425a712267c1
Umeshk-1086 <2403051050703@paruluniversity.ac.in>
2025-12-04 19:17:45 +0530

    Add files via upload

---
+++ numgenrater/numgen.js
@@ -0,0 +1,7 @@
+function generateRandomNumber() {
+    var min = 1;
+    var max = 100;
+    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    document.getElementById('randomNumber').innerText = randomNumber;
+}
